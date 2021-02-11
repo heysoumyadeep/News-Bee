f9ef8b5e202bfa2e6dc87de54896a1b239d0db46
Soumyadeep Pradhan <soumyadeeppradhan@gmail.com>
2021-02-11 16:03:57 +0530

    Initial Commit

---
+++ app/src/main/java/com/soumyadeeppradhan/newsbee/IntroScreen.kt
@@ -0,0 +1,18 @@
+package com.soumyadeeppradhan.newsbee
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class IntroScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
---
+++ app/src/main/java/com/soumyadeeppradhan/newsbee/News.kt
@@ -0,0 +1,8 @@
+package com.soumyadeeppradhan.newsbee
+
+data class News(
+    val title: String,
+    val author: String,
+    val url: String,
+    val imageUrl: String
+)
